214d22355c11d3e8dff81011ba7b51907cb28f13
Riyanshu Sharma <riyanshu062@gmail.com>
2025-07-09 01:55:54 +0530

    Middle Of Three

---
+++ S02_Mathematics/MiddleOfThree.java
@@ -0,0 +1,17 @@
+//Given three distinct numbers a, b and c. Find the number with a value in the middle
+
+package S02_Mathematics;
+
+public class MiddleOfThree {
+
+    int middle(int a, int b, int c) {
+        
+        if ((b < a && a < c) || (c < a && a < b)) {
+            return a;
+        } else if ((a < b && b < c) || (c < b && b < a)) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+}
