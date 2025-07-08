cd4892fdfd26e9498e4f68197df1325886d39958
Riyanshu Sharma <riyanshu062@gmail.com>
2025-07-09 02:45:21 +0530

    Gp

--- S02_Mathematics/SumOfSeriesOfGP.java
+++ S02_Mathematics/SumOfSeriesOfGP.java
@@ -10,12 +10,11 @@ public class SumOfSeriesOfGP {
         if (r == 1) {
             return n * a;
         } else {
-            // Calculate r^n
-            long r_power_n = 1;
+            long r_power_n = 1;  // Calculate r^n
             for (int i = 0; i < n; i++) {
                 r_power_n *= r;
             }
-            return a * (r_power_n - 1) / (r - 1);
+            return a * (1 - r_power_n) / (1 - r);
         }
     }
 }
