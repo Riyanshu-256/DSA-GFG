6b5e99d36cdfcbb4e78f4083c7aebba2e83244c4
Riyanshu Sharma <riyanshu062@gmail.com>
2025-07-09 01:46:49 +0530

    Greatest Of Three Numbers

---
+++ S02_Mathematics/GreatestOfThreeNumbers.java
@@ -0,0 +1,20 @@
+// Given 3 numbers a, b and c. Find the greatest number among them.
+
+package S02_Mathematics;
+
+public class GreatestOfThreeNumbers {
+    public int greatestOfThree(int a, int b, int c) {
+        // code here
+        if(a>=b && a>=c)
+        {
+            return a;
+        }
+        else if(b>=c && b>=a){
+            return b;
+        }
+        else 
+        {
+            return c;
+        }
+    }
+}
