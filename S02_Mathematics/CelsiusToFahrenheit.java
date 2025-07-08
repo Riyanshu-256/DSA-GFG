3b889c2aef0a78e94c8cc52d4dabc3cb8107df29
Riyanshu Sharma <riyanshu062@gmail.com>
2025-07-09 02:25:50 +0530

    CelsiusToFahrenheit

---
+++ S02_Mathematics/CelsiusToFahrenheit.java
@@ -0,0 +1,9 @@
+package S02_Mathematics;
+
+public class CelsiusToFahrenheit {
+    static double celsiusToFahrenheit(int C) {
+        // code here
+        double F = (C * 9.0 / 5.0) + 32.0;
+        return F;
+    }
+}
