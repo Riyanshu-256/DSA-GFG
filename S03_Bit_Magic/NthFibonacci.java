212224dc72c9be62659a12c115ec6847457c1b0a
Riyanshu Sharma <riyanshu062@gmail.com>
2025-07-10 13:16:50 +0530

    Updated

--- S03_Bit_Magic/NthFibonacci.java
+++ S03_Bit_Magic/NthFibonacci.java
@@ -8,12 +8,12 @@ package S03_Bit_Magic;
 public class NthFibonacci {
     static int fib(int N) {
         // code here
-        if (N == 0)
+        if (N == 0){
         return 0;
-        
-        if (N == 1)
+        }
+        if (N == 1){}
         return 1;
-
+        }
         int a = 0, b = 1;
         for (int i = 2; i <= N; i++) {
             int c = (a + b) % 10;
