4d7223ef4bf9fcf36ab221b31a39fd61dd685f11
Riyanshu Sharma <riyanshu062@gmail.com>
2025-07-11 20:44:24 +0530

    Square Root

---
+++ S02_Mathematics/SqrRoot.java
@@ -0,0 +1,12 @@
+/* Given a positive integer n, find the square root of n. If n is not a perfect square, then return the floor value.
+Floor value of any number is the greatest Integer which is less than or equal to that number
+*/
+package S02_Mathematics;
+
+public class SqrRoot {
+    int floorSqrt(int n) {
+        // code here
+        double sqrRoot = Math.sqrt (n);
+        return (int) Math.floor(sqrRoot);
+    }
+}
