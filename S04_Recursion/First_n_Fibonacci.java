f33e22baf3370f427fe8f60c04c9db4f1680800f
Riyanshu Sharma <riyanshu062@gmail.com>
2025-07-14 21:54:49 +0530

    First n Fibonacci

---
+++ S04_Recursion/First_n_Fibonacci.java
@@ -0,0 +1,17 @@
+package S04_Recursion;
+
+public class First_n_Fibonacci {
+    public static int[] fibonacciNumbers(int n) {
+        // Your code here
+         int[] fib = new int[n];
+        
+        if (n >= 1) fib[0] = 0;
+        if (n >= 2) fib[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        return fib;
+    }
+}
