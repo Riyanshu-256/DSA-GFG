7d9f8dd06f92e49f124d93f5368b91dc660dc59b
Riyanshu Sharma <riyanshu062@gmail.com>
2025-07-15 22:53:36 +0530

    Array Isert at End

---
+++ S05_Array/ArrayInsertAtIndex.java
@@ -0,0 +1,8 @@
+package S05_Array;
+import java.util.ArrayList;
+
+public class ArrayInsertAtIndex {
+    void insertAtIndex(ArrayList<Integer> arr, int index, int val) {
+        arr.add(index, val);
+    }
+}
