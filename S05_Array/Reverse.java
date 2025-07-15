6f759a9e2406b405822313cdd74907cc55187864
Riyanshu Sharma <riyanshu062@gmail.com>
2025-07-15 23:08:47 +0530

    Reverse

---
+++ S05_Array/Reverse.java
@@ -0,0 +1,16 @@
+//You are given an array of integers arr[]. Your task is to reverse the given array.
+
+package S05_Array;
+public class Reverse {
+     public void reverseArray(int arr[]) {
+        // code here
+        int low = 0, high =arr.length -1; 
+        while(low<high){
+            int temp = arr [low];
+            arr[low] = arr [high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
+}
