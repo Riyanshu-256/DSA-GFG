c04eeb3581192bb975441b3a8bb70573f6295d8b
Riyanshu Sharma <riyanshu062@gmail.com>
2025-07-16 23:13:12 +0530

    Trapping Rain Water

---
+++ S05_Array/Solution.java
@@ -0,0 +1,10 @@
+package S05_Array;
+
+public class Solution {
+
+    public String maxWater(int[] arr) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'maxWater'");
+    }
+
+}
