fb263dfa0db6fe44bab77ecec8ebcadef34a7572
Riyanshu Sharma <riyanshu062@gmail.com>
2025-07-19 23:47:28 +0530

    Sum of digits

---
+++ S02_Mathematics/SumOfDigits.java
@@ -0,0 +1,13 @@
+package S02_Mathematics;
+
+public class SumOfDigits {
+    public static int digit(int n){
+        int sum =0;
+        while(n>0){
+            int lastDigit= n%10;
+            sum = sum+lastDigit;
+            n=n/10;
+        }
+        return(sum);
+    }
+}
