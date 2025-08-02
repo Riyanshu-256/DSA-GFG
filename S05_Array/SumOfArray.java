b05fbe4045767fdb7e3d8e24dd90e1eda8fdd003
Riyanshu Sharma <riyanshu062@gmail.com>
2025-08-02 14:33:50 +0530

    SumOfArray

---
+++ S05_Array/SumOfArray.java
@@ -0,0 +1,16 @@
+/*
+You are given an integer array arr[]. The task is to find the sum of it
+*/
+package S05_Array;
+
+public class SumOfArray {
+    int arraySum(int arr[]) {
+        // code here
+        int n = arr.length;
+        int sum = 0;
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+        }
+        return sum;
+    }
+}
