10da64c22f51e6fa5147d98016786d49626c310b
Riyanshu Sharma <riyanshu062@gmail.com>
2025-08-21 23:26:32 +0530

    done

---
+++ S07_Sorting/BubbleSort.java
@@ -0,0 +1,18 @@
+package S07_Sorting;
+
+public class BubbleSort {
+    public void bubbleSort(int[] arr) {
+        // code here
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap 
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
