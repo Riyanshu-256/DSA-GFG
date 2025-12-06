8614b51df79f4eabc3092179bca4c746a4763792
Anshu <riyanshu062@gmail.com>
2025-12-06 20:12:39 +0530

    Palindrome String

--- S04_Recursion/Palindrome.java
+++ S04_Recursion/Palindrome.java
@@ -7,5 +7,16 @@ A number is considered a palindrome if it reads the same backward as forward, li
 package S04_Recursion;
 
 public class Palindrome {
+    boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        return check(s, start, end);
+    }
     
+    boolean check(String s, int start, int end) {
+        if (start >= end) return true;
+        
+        return (s.charAt(start) == s.charAt(end)) 
+                && check(s, start + 1, end - 1);
+    }
 }
