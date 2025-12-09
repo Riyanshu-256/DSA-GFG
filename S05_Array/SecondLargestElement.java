45986b61b0999263bfce4f1ac1ef9f9d3daef370
Anshu <riyanshu062@gmail.com>
2025-12-09 17:54:03 +0530

    Second Largest Element

---
+++ S05_Array/SecondLargestElement.java
@@ -0,0 +1,25 @@
+public class SecondLargestElement {
+    public int getSecondLargest(int[] arr) {
+        // code here
+        int n = arr.length;
+
+        if(n < 2) return -1;
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for(int i = 0; i < n; i++){
+            if(arr[i] > largest){
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest){
+                secondLargest = arr[i];
+            }
+        }
+
+        if(secondLargest == Integer.MIN_VALUE)
+            return -1;
+
+        return secondLargest;
+    }
+}
