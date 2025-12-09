cc16f43249d38f5131dc28cb130a687ba03c2424
Anshu <riyanshu062@gmail.com>
2025-12-09 18:43:22 +0530

    Rotate array by one

--- S05_Array/RotateArrayByOne.java
+++ S05_Array/RotateArrayByOne.java
@@ -9,8 +9,8 @@ public class RotateArrayByOne {
         // code here
         int n = arr.length;
         int temp = arr[n-1];
-        
-        for(int i = n-1; i > 0; i--){
+
+        for(int i = n - 1; i > 0; i--) {
             arr[i] = arr[i - 1];
         }
         arr[0] = temp;
