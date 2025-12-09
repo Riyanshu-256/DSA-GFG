c805461157b35dc569525cfe62a89abd1fd3e0c7
Anshu <riyanshu062@gmail.com>
2025-12-09 17:57:38 +0530

    Binary Search

---
+++ S05_Array/BinarySearch.java
@@ -0,0 +1,10 @@
+public class BinarySearch {
+    public int binarysearch(int[] arr, int k) {
+        // Code Here
+        int n = arr.length;
+        for(int i=0; i<n; i++){
+            if(arr[i] == k) return i;
+        }
+        return -1;
+    }
+}
