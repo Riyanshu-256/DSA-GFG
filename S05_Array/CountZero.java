6ebb2988ab5ee505430a9e978db17e8e36265827
Anshu <riyanshu062@gmail.com>
2025-12-10 18:07:47 +0530

    Count Zero

---
+++ S05_Array/CountZero.java
@@ -0,0 +1,14 @@
+public class CountZero {
+    int countZeroes(int[] arr) {
+        // code here
+        int n = arr.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0)
+                count++;
+        }
+
+        return count;
+    }
+}
